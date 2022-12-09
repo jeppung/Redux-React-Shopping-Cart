b74fbf86816bcb64407553b2560bb3922a7b0ea5
Joshua Pulung <jeppung@gmail.com>
2022-12-09 15:47:10 +0700

    Initial Commit

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./Header";
 import Products from "./Products";
 import "./Layout.css";
+import CartItems from "./CartItems";
+import { useSelector } from "react-redux";
 const Layout = () => {
-  let total = 100;
+  const itemsList = useSelector((state) => state.cart.itemsList);
+  let total;
+
+  try{
+    total = Array.from(itemsList).map(({totalPrice}) => totalPrice).reduce((a,b) => a + b); 
+  }catch{
+    total = 0;
+  }
+  
+  const showCart = useSelector((state) => state.cart.showCart);
+  
 
   return (
     <React.Fragment>
       <div className="layout">
         <Header />
         <Products />
+        {showCart && <CartItems />}
         <div className="total-price">
           <h3>Total: ${total}</h3>
           <button className="orderBtn">Place Order</button>
